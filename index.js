278669f3c75b9f504c6ecab568487c04c4392bdf
miiv0 <minzana2002@gmail.com>
2026-06-18 02:27:29 -0400

    add functions and add improvements to each function for autofill

--- index.js
+++ index.js
@@ -4,7 +4,58 @@ const cityDataList = document.getElementById("cityList")
 const cityPromise = fetch("./cities500.json")
 const cityResponse = await cityPromise
 
-const cityList = (await cityResponse.json()).slice(0, 50)
+let cityList = await cityResponse.json()
+
+console.log(cityList)
+console.log(cityList["New York"])
+
+// function combineCityNames() {
+//     const cityNames = []
+//     const NY = cityList["New York"]
+//     const AL = cityList["Alabama"]
+//     const TX = cityList["Texas"]
+//     for (let i = 0; i < NY.length; i++) {
+//         const nyCities = NY[i]
+//         cityNames.push(nyCities)
+//     }
+//     for (let i = 0; i < AL.length; i++) {
+//         const alCities = AL[i]
+//         cityNames.push(alCities)
+//     }
+//     for (let i = 0; i < TX.length; i++) {
+//         const txCities = TX[i]
+//         cityNames.push(txCities)
+//     }
+//     return cityNames
+// }
+
+// function combineCityNames() {
+//     const cityNames = []
+//     const NY = cityList["New York"]
+//     const AL = cityList["Alabama"]
+//     const TX = cityList["Texas"]
+//     const cityNamesCombined = cityNames.concat(NY).concat(AL).concat(TX)
+//     return cityNamesCombined
+// }
+
+// function combineCityNames() {
+//     const NY = cityList["New York"]
+//     const AL = cityList["Alabama"]
+//     const TX = cityList["Texas"]
+//     return [...NY, ...AL, ...TX]
+// }
+
+function combineCityNames() {
+    const cityNames = []
+    for (const state in cityList) {
+        const stateCities = cityList[state]
+        cityNames.push(...stateCities)
+    }
+    return cityNames
+}
+
+cityList = combineCityNames()
+console.log(cityList)
 
 // function getCityNames() {
 //     const cityNames = []
