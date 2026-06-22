598d93dafdfc1c25defa10ebc0b365b13668acb7
miiv0 <minzana2002@gmail.com>
2026-06-22 19:03:19 -0400

    add reset upon click button

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function cityDatasetOptions(cityNames) {
 weatherBtn.addEventListener("click", function () {
     const key = `PJZ3FXN2MGCFQZ6B6BR3K34FE`
     const api = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${(locationQuery.value)}?unitGroup=us&key=${key}&contentType=json`
-
+    weatherResult.innerHTML = ''
     console.log(locationQuery.value)
     console.log(api)
     fetch(encodeURI(api))
